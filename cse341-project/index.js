6a85b9bd1765009d986c1ce155fcf284792de838
edwardsja <edw15013@byui.edu>
2021-09-25 11:58:40 -0400

    first commit

--- cse341-project/index.js
+++ cse341-project/index.js
@@ -12,7 +12,6 @@
  *******************************************************************************/
 // Our initial setup (package requires, port number setup)
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
@@ -20,33 +19,40 @@ const app = express();
 
 // Route setup. You can implement more in the future!
 const ta01Routes = require('./routes/ta01');
+
+//week2
 const ta02Routes = require('./routes/ta02');
+const w2_adminData = require('./routes/prove02-admin');
+const w2_shopRoutes = require('./routes/prove02-shop');
+
+// week 3
 const ta03Routes = require('./routes/ta03');
+
+// week 4
 const ta04Routes = require('./routes/ta04');
+
 
 app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  // For view engine as Pug
-  //.set('view engine', 'pug') // For view engine as PUG.
-  // For view engine as hbs (Handlebars)
-  //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
-  //.set('view engine', 'hbs')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+  .use(express.urlencoded({extended: true}))
+  .use(express.json()) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
+  .use('/week2', w2_shopRoutes)
+  .use('/week2/admin', w2_adminData.routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
-    res.render('pages/index', {
+    res.render('index', {
       title: 'Welcome to my CSE341 repo',
       path: '/',
     });
   })
   .use((req, res, next) => {
     // 404 page
-    res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
+    res.render('404', { title: '404 - Page Not Found', path: req.url });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ cse341-project/routes/prove02-shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+const adminData = require('./prove02-admin');
+
+
+
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('prove02-shop', {
+        prods: products,
+        title: 'Shop',
+        path: '/week2'
+    })
+});
+
+module.exports = router;
