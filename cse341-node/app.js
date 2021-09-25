33a81fe9e0891e197dabb07966e08388d990317a
edwardsja <edw15013@byui.edu>
2021-09-25 12:16:24 -0400

    week 2 complete

--- cse341-node/app.js
+++ cse341-node/app.js
@@ -1,6 +1,23 @@
-const http = require('http');
-const routes = require('./routes');
- 
-const server = http.createServer(routes.handler);
+const path = require('path');
 
-server.listen(3000);
+const express = require('express');
+//const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './cse341-node/views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({extended: true}))
+    .use(express.json())
+    .use(express.static(path.join(__dirname, 'public')))
+    .use('/admin', adminData.routes)
+    .use(shopRoutes)
+    .use((req, res, next) => {
+        res.status(404).render('404', { title: 'Page Not Found' });
+    })
+    .listen(3000);
+ 
